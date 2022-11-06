8e430f5990b70debd79c1bdf40b9173727f79407
zhangzhiyu1994 <zhiyu.zhang@cqbdri.pku.edu.cn>
2022-11-06 15:23:13 +0800

    feat:段错误信号捕捉

--- src/signal_test_case.cpp
+++ src/signal_test_case.cpp
@@ -1,12 +1,8 @@
 /**
- * @brief 功能测试
+ * @brief 段错误等信号测试
  * @author zhangzhiyu
- * @date 2022-09-16
+ * @date 2022-11-06
  */
-
-#include "tools/tassk_class.h"
-#include "tools/msg_service.h"
-#include "tools/table_driven.h"
 
 #include <utility>
 #include <iostream>
@@ -15,6 +11,8 @@
 #include <cmath>
 #include <cstdint>
 #include <cstring>
+#include <vector>
+
 void signal_callback(int x) {
     std::cout << "signal test" << std::endl;
 }
@@ -32,29 +30,9 @@ private:
     int a_ = 0;
 };
 
-#define ERR_T std::uint32_t
-
-#define FEM_ERROR_SUCCESS 0;
-
 int main()
 {
-    ERR_T abc = 1;
-    if (abc == FEM_ERROR_SUCCESS) {
-        std::cout << "FEM_ERROR_SUCCESS" << std::endl;
-    }
-
-    double f1 = std::nan("NaN");
-    std::array<double, 6> value_{f1};
-    value_.fill(f1);
-    double x = f1 * f1 + 1;
-    double y = f1/2;
-
-    std::cout << "f1: " << f1 << " x: " << x << " y: " << y << std::endl;
-
-
     std::cout << "SSSSSSSSSSSSSSSSSSSSSSSSSSS" << std::endl;
-
-
 
     signal(SIGSEGV, signal_callback);
     std::vector<int> arr{1, 2, 3};
