7d733ec13a348193c304e53d36b0037bfcd0a050
zhangzhiyu1994 <zhiyu.zhang@cqbdri.pku.edu.cn>
2023-05-15 10:05:38 +0800

    feat: vtk切块及切面功能

--- src/temp_test.cpp
+++ src/temp_test.cpp
@@ -1,112 +1,292 @@
-#include <vtkImageViewer2.h>
+/**
+ * @file show_deformed_contour.cpp
+ * @author LiuShuai (you@domain.com)
+ * @brief 显示切割后的图形
+ * @date 2023-1-2
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <vtkActor.h>
+#include <vtkActor2D.h>
+#include <vtkAreaPicker.h>
+#include <vtkArrowSource.h>
+#include <vtkAxesActor.h>
+#include <vtkCamera.h>
+#include <vtkCellCenters.h>
+#include <vtkCellData.h>
+#include <vtkClipDataSet.h>
+#include <vtkCutter.h>
+#include <vtkCylinderSource.h>
+#include <vtkDataSetMapper.h>
+#include <vtkDoubleArray.h>
+#include <vtkGlyph3D.h>
+#include <vtkHexahedron.h>
+#include <vtkIdFilter.h>
+#include <vtkIdTypeArray.h>
+#include <vtkIndent.h>
+#include <vtkInteractorStyleRubberBandPick.h>
+#include <vtkLabeledDataMapper.h>
+#include <vtkLight.h>
+#include <vtkLongArray.h>
+#include <vtkLookupTable.h>
 #include <vtkNamedColors.h>
 #include <vtkNew.h>
-#include <vtkPNGReader.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkRenderer.h>
-#include <vtkImageActor.h>
-#include <vtkImageCast.h>
-#include <vtkImageGridSource.h>
-#include <vtkImageMapper3D.h>
-#include <vtkInteractorStyleImage.h>
-#include <vtkPolyDataMapper.h>
-#include <vtkTexture.h>
-#include <vtkImageData.h>
-#include <vtkJPEGReader.h>
-#include <vtkNamedColors.h>
-#include <vtkNew.h>
-#include <vtkPlaneSource.h>
+#include <vtkObjectFactory.h>
+#include <vtkPlane.h>
+#include <vtkPlaneWidget.h>
+#include <vtkPlanes.h>
+#include <vtkPointData.h>
+#include <vtkPointSource.h>
+#include <vtkPoints.h>
 #include <vtkPolyData.h>
 #include <vtkPolyDataMapper.h>
+#include <vtkPolyVertex.h>
 #include <vtkProperty.h>
+#include <vtkProperty2D.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
-#include <vtkTexture.h>
-#include <vtkTextureMapToPlane.h>
-#include <vtkOrientationMarkerWidget.h>
+#include <vtkRendererCollection.h>
+#include <vtkScalarBarActor.h>
+#include <vtkSmartPointer.h>
+#include <vtkSphereSource.h>
+#include <vtkTextProperty.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkVersion.h>
+#include <vtkVertex.h>
+#include <vtkVertexGlyphFilter.h>
+#include <math.h>
+using namespace std;
 
-
-
-int main()
+namespace
 {
-    vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
-    vtkNew<vtkNamedColors> colors;
-    std::string inputFilename = "D:\\work_station\\befem-studio\\share\\befem-studio.png";
 
-    // Read the image
-    vtkNew<vtkPNGReader> reader;
-    reader->SetFileName(inputFilename.c_str());
-
-    // 方案一：不可行————vtkImageViewer2接管了所有渲染流程，不兼容其他actor显示
-    {
-        //   vtkNew<vtkImageViewer2> imageViewer;
-        //   imageViewer->SetInputConnection(reader->GetOutputPort());
-        //   vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
-        //   imageViewer->SetupInteractor(renderWindowInteractor);
-        //   imageViewer->Render();
-        //   imageViewer->GetRenderer()->ResetCamera();
-        //   imageViewer->GetRenderer()->SetBackground(
-        //       colors->GetColor3d("DarkSlateGray").GetData());
-        //   imageViewer->GetRenderWindow()->SetWindowName("PNGReader");
-        //   imageViewer->Render();
+// Define interaction style
+class InteractorStyle : public vtkInteractorStyleRubberBandPick {
+public:
+    static InteractorStyle *New();
+    vtkTypeMacro(InteractorStyle, vtkInteractorStyleRubberBandPick);
+    //构造函数
+    InteractorStyle() {
+        // this->ArrawActor = vtkSmartPointer<vtkActor>::New();
+        newPoints = vtkSmartPointer<vtkPoints>::New();
+    }
+    void UpdatePlaneAndWidget() {
+        PlaneWidget->GetPlane(Plane);
+        // PlaneWidget->SetCenter(Plane->get()); //
+        // PlaneWidget->PlaceWidget(UnstructuredGrid->GetBounds()); //
     }
 
-    // 方案二：vtkImageActor显示图片-----图片大小可以缩放旋转
-    // // Create an actor
-    // vtkNew<vtkImageActor> actor;
-    // actor->GetMapper()->SetInputConnection(reader->GetOutputPort());
+    virtual void OnLeftButtonUp() override {
+        vtkNew<vtkNamedColors> colors;
 
-    // 方案三：vtkImageActor显示图片-----图片大小可以缩放旋转
+        // Forward events
+        vtkInteractorStyleRubberBandPick::OnLeftButtonUp();
 
-    // Create a plane
-    vtkNew<vtkPlaneSource> plane;
-    plane->SetCenter(0.0, 0.0, 0.0);
-    plane->SetNormal(0.0, 0.0, 1.0);
-    // Apply the texture
-    vtkNew<vtkTexture> texture;
-    texture->SetInputConnection(reader->GetOutputPort());
-    vtkNew<vtkTextureMapToPlane> texturePlane;
-    texturePlane->SetInputConnection(plane->GetOutputPort());
-    vtkNew<vtkPolyDataMapper> planeMapper;
-    planeMapper->SetInputConnection(texturePlane->GetOutputPort());
-    vtkNew<vtkActor> texturedPlane;
-    texturedPlane->SetMapper(planeMapper);
-    texturedPlane->SetTexture(texture);
-    texturedPlane->GetProperty()->SetColor(colors->GetColor3d("MistyRose").GetData());
-    // Visualize the textured plane
-    renderer->AddActor(texturedPlane);
+        // 切割(开始)--lius
+        UpdatePlaneAndWidget();
+        // 切割(结束)--lius
+        this->GetInteractor()->GetRenderWindow()->Render();
+        this->HighlightProp(NULL);
+    }
+    virtual void OnRightButtonUp() override {
+        // 原始actor的隐藏/显示
+        vtkInteractorStyleRubberBandPick::OnRightButtonUp();
+        this->Actor->GetVisibility() ? (Actor->VisibilityOff()) : Actor->VisibilityOn();
 
+        this->GetInteractor()->GetRenderWindow()->Render();
+    }
 
-    // 可视化流程___________________________________________________________________________________
-    renderer->ResetCamera();
-    renderer->SetBackground(colors->GetColor3d("CornflowerBlue").GetData());
-    // Setup render window
-    vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+    void SetGrid(vtkSmartPointer<vtkUnstructuredGrid> grid) {
+        this->UnstructuredGrid = grid;
+        this->ConstUnstructuredGrid->DeepCopy(grid);
+    }
+    void SetActor(vtkSmartPointer<vtkActor> actor) {
+        this->Actor = actor;
+    }
+    void SetPlane(vtkSmartPointer<vtkPlane> plane) {
+        this->Plane = plane;
+    }
+    void SetWidget(vtkSmartPointer<vtkPlaneWidget> planeWidget) {
+        this->PlaneWidget = planeWidget;
+    }
+
+private:
+    vtkSmartPointer<vtkUnstructuredGrid> UnstructuredGrid;
+    vtkSmartPointer<vtkUnstructuredGrid> ConstUnstructuredGrid = vtkSmartPointer<vtkUnstructuredGrid>::New();
+    vtkSmartPointer<vtkActor> Actor;
+    vtkSmartPointer<vtkPoints> newPoints;
+    vtkSmartPointer<vtkPlane> Plane;
+    vtkSmartPointer<vtkPlaneWidget> PlaneWidget;
+};
+
+vtkStandardNewMacro(InteractorStyle);
+} // namespace
+
+int main(int, char *[]) {
+    vtkNew<vtkNamedColors> colors;
+
+    // 刘帅自定义数据源 vtkUnstructuredGrid 27-point 8-cell
+    vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+
+    for (int y = -1; y <= 1; y++) //循环y,-1到1
+    {
+        for (int z = 1; z >= -1; z--) //循环z,1到-1
+        {
+            for (int x = -1; x <= 1; x++) //循环x,-1到1
+            {
+                points->InsertNextPoint(x, y, z);
+            }
+        }
+    }
+    // cout << "points num: " << points->GetNumberOfPoints() << endl;
+    //模拟标量数据，暂定为结点y坐标
+    vtkSmartPointer<vtkDoubleArray> doubleArray = vtkSmartPointer<vtkDoubleArray>::New();
+    for (size_t i = 0; i < points->GetNumberOfPoints(); i++) {
+        doubleArray->InsertNextTuple1(points->GetPoint(i)[1]); //每个结点的y坐标
+    }
+
+    vtkSmartPointer<vtkUnstructuredGrid> grid = vtkSmartPointer<vtkUnstructuredGrid>::New();
+    {                             //27个结点组成八个单元
+        int num[] = {0, 1, 4, 3}; // 1 2 5 4,减一
+        int add[] = {0, 1, 3, 4, 9, 10, 12, 13};
+        vector<vector<int>> cells; //存8个序列
+        const int dif = 9;
+        for (size_t i = 0; i < 8; i++) {
+            vector<int> elementNumber;
+            vtkSmartPointer<vtkHexahedron> hex = vtkSmartPointer<vtkHexahedron>::New();
+            // 形成每个单元的结点编号
+            for (size_t j = 0; j < 8; j++) {
+                if (j < 4) {
+                    elementNumber.push_back(num[j] + add[i]);
+                } else {
+                    elementNumber.push_back(num[j - 4] + dif + add[i]);
+                }
+            }
+            // 利用这些结点编号，插入单元中
+            for (size_t j = 0; j < 8; j++) {
+                hex->GetPointIds()->SetId(j, elementNumber[j]);
+            }
+
+            cells.push_back(elementNumber);
+            grid->InsertNextCell(hex->GetCellType(), hex->GetPointIds()); //插入单元
+        }
+    }
+
+    grid->SetPoints(points);
+    grid->GetPointData()->SetScalars(doubleArray);
+    // 颜色映射表
+    vtkNew<vtkLookupTable> pColorTableModel;
+    pColorTableModel->SetNumberOfColors(12);
+    pColorTableModel->SetHueRange(0.67, 0); //色调从红色到蓝色，【0，0.67】则表示从蓝色到红色
+    pColorTableModel->SetAlphaRange(1, 1);
+    pColorTableModel->SetValueRange(1, 1);
+    pColorTableModel->SetSaturationRange(1, 1);
+    pColorTableModel->SetTableRange(0, 1); //最大值最小值
+    pColorTableModel->Build();
+    // vtkDataSetMapper
+    vtkNew<vtkDataSetMapper> dataSetMapper;
+    dataSetMapper->SetInputData(grid);
+    dataSetMapper->SetLookupTable(pColorTableModel);
+    dataSetMapper->SetInterpolateScalarsBeforeMapping(1);
+    dataSetMapper->SetScalarRange(0, 1); //最大值最小值
+
+    //渲染网格的色卡
+    vtkSmartPointer<vtkActor> actorGrid = vtkSmartPointer<vtkActor>::New();
+    actorGrid->SetMapper(dataSetMapper);
+    actorGrid->GetProperty()->SetColor(1, 0, 0);
+    // actorGrid->GetProperty()->SetOpacity(0.5);
+    actorGrid->GetProperty()->SetLineWidth(1);
+    actorGrid->GetProperty()->SetEdgeVisibility(1);
+
+    // 设置scalarBar（色卡）
+    vtkNew<vtkScalarBarActor> scalarBarActor;
+    scalarBarActor->SetLookupTable(pColorTableModel);
+
+    scalarBarActor->SetTitle("Height");
+    scalarBarActor->GetTitleTextProperty()->ShadowOff(); //“标题”关闭阴影
+    scalarBarActor->GetLabelTextProperty()->ShadowOff(); //“标签”关闭阴影
+    scalarBarActor->GetTitleTextProperty()->ItalicOff(); //“标题”关闭斜体
+    scalarBarActor->GetLabelTextProperty()->ItalicOff(); //“标签”关闭斜体
+    scalarBarActor->GetTitleTextProperty()->BoldOff();   //“标题”不加粗
+    scalarBarActor->GetLabelTextProperty()->BoldOff();   //“标签”不加粗
+    scalarBarActor->SetNumberOfLabels(13);
+
+    // Visualize
+    vtkNew<vtkRenderer> renderer;
+    vtkNew<vtkRenderWindow> renderWindow;
     renderWindow->AddRenderer(renderer);
-    renderWindow->SetWindowName("ImageGridSource");
-    // Setup render window interactor
-    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-    // Render and start interaction
+    renderWindow->SetWindowName("show_deformed_contour");
+    renderWindow->SetSize(900, 600);
+    renderer->AddActor(scalarBarActor);
+    renderer->AddActor(actorGrid);
+
+    renderer->SetBackground(colors->GetColor3d("DarkSlateGray").GetData());
+    renderer->ResetCamera(actorGrid->GetBounds()); //重置
+    // 创建交互器
+    vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
     renderWindowInteractor->SetRenderWindow(renderWindow);
 
+    //widget
+    // 创建交互器和切面小部件
 
+    vtkSmartPointer<vtkPlaneWidget> planeWidget = vtkSmartPointer<vtkPlaneWidget>::New();
+    planeWidget->SetInteractor(renderWindowInteractor);
+    planeWidget->SetResolution(20);
+    planeWidget->GetPlaneProperty()->SetColor(1, 1, 1);
+    planeWidget->SetRepresentationToOutline();   //边框
+    planeWidget->SetCenter(grid->GetCenter());   //放在中间位置
+    planeWidget->NormalToYAxisOn();              //指向Y轴
+    planeWidget->PlaceWidget(grid->GetBounds()); //放置widget
+    planeWidget->SetEnabled(1);
+    planeWidget->On();
 
+    //提前创建好平面
+    vtkSmartPointer<vtkPlane> plane = vtkSmartPointer<vtkPlane>::New();
+    planeWidget->GetPlane(plane);
 
+    // 创建切割对象
+    vtkSmartPointer<vtkCutter> cutter = vtkSmartPointer<vtkCutter>::New();
+    cutter->SetInputData(grid);
+    cutter->SetCutFunction(plane);
 
+    // cutter->GetValues
+    // 创建切面演员
+    vtkSmartPointer<vtkPolyDataMapper> cutterMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    cutterMapper->SetLookupTable(pColorTableModel);
+    cutterMapper->SetInputConnection(cutter->GetOutputPort());
+    cutterMapper->SetInterpolateScalarsBeforeMapping(1);
+    cutterMapper->SetScalarRange(0, 1); //最大值最小值
+    vtkSmartPointer<vtkActor> cutterActor = vtkSmartPointer<vtkActor>::New();
+    cutterActor->SetMapper(cutterMapper);
+    // cutterActor->GetProperty()->SetColor(1, 0, 0);
 
+    // 将切面演员添加到渲染器中
+    renderer->AddActor(cutterActor);
 
+    //自定义交互方式+传参
+    vtkNew<InteractorStyle> style;
+    // style->StartSelect(); // 开始选择按键的设置
+    style->SetGrid(grid);
+    style->SetActor(actorGrid);
+    style->SetPlane(plane);
+    style->SetWidget(planeWidget);
+    renderWindowInteractor->SetInteractorStyle(style);
 
+    vtkSmartPointer<vtkLight> light = vtkSmartPointer<vtkLight>::New();
+    light->SetLightTypeToCameraLight();
 
-    // auto axisWidget_ = vtkSmartPointer<vtkOrientationMarkerWidget>::New();
-    // axisWidget_->SetOrientationMarker(texturedPlane);
-    // axisWidget_->SetInteractor(renderWindowInteractor);
-    // axisWidget_->SetEnabled(true);
-    // axisWidget_->SetInteractive(true);
-
-        renderWindow->Render();
-    renderWindowInteractor->Initialize();
+    //开始交互
+    renderWindow->Render();
     renderWindowInteractor->Start();
-    return 0;
+
+    return EXIT_SUCCESS;
 }
---
+++ src/vtk/slice_and_cutter/cutter_test.cpp
@@ -0,0 +1,156 @@
+/**
+ * @brief 采用vtkPlane做为要获取的切面，
+ * 将vtkPlane作为vtkCutter的参数来获取被切actor上指定位置的切面
+ * 
+ * 此例子演示了在外轮廓面上，用切面获取切割后的外轮廓线
+ * @author zhangzhiyu
+ * @date 2023-01-05
+ */
+
+/**
+ * @brief 此例子展示了cutter(切面功能)
+ */
+
+// 切块
+// vtkClipDataSet
+// vtkClipPoloData
+// 基于Qt 和 VTK 的铸造有限元后处理可视化系统研发
+
+// 包围盒：cubeAxesActor
+// 基于Qt和VTK的铸造有限元后处理可视化系统研发_陈航.pdf
+// 1、3.6.3     剖切--46页
+// 2、4.2.4     铸件的剖切--60页
+
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkCellArray.h>
+#include <vtkCutter.h>
+#include <vtkDataSetMapper.h>
+#include <vtkDataSetSurfaceFilter.h>
+#include <vtkHexahedron.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkPlane.h>
+#include <vtkPoints.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkUnstructuredGrid.h>
+
+int main()
+{
+    vtkNew<vtkNamedColors> colors;
+
+
+    // Setup the coordinates of eight points
+    // (the two faces must be in counter clockwise order as viewed from the
+    // outside)
+    // 单元1的节点
+    std::array<std::array<double, 3>, 8> pointCoords{{{{0.0, 0.0, 0.0}},
+                                                      {{1.0, 0.0, 0.0}},
+                                                      {{1.0, 1.0, 0.0}},
+                                                      {{0.0, 1.0, 0.0}},
+                                                      {{0.0, 0.0, 1.0}},
+                                                      {{1.0, 0.0, 1.0}},
+                                                      {{1.0, 1.0, 1.0}},
+                                                      {{0.0, 1.0, 1.0}}}};
+
+    // Create the points and a hexahedron from the points.
+    vtkNew<vtkPoints> points;
+    vtkNew<vtkHexahedron> hexa;
+    for (auto i = 0; i < pointCoords.size(); ++i)
+    {
+        points->InsertNextPoint(pointCoords[i].data());
+        hexa->GetPointIds()->SetId(i, i);
+    }
+
+    // 单元2的节点
+    std::array<std::array<double, 3>, 8> pointCoords2{{{{1.0, 0.0, 0.0}},
+                                                       {{2.0, 0.0, 0.0}},
+                                                       {{2.0, 1.0, 0.0}},
+                                                       {{1.0, 1.0, 0.0}},
+                                                       {{1.0, 0.0, 1.0}},
+                                                       {{2.0, 0.0, 1.0}},
+                                                       {{2.0, 1.0, 1.0}},
+                                                       {{1.0, 1.0, 1.0}}}};
+
+    // Create the points and a hexahedron from the points.
+    vtkNew<vtkHexahedron> hexa2;
+    for (auto i = 0; i < pointCoords2.size(); ++i)
+    {
+        points->InsertNextPoint(pointCoords2[i].data());
+        hexa2->GetPointIds()->SetId(i, i + 8);
+    }
+
+    // 将两个单元放在网格对象中
+    // Add the points and hexahedron to an unstructured grid.
+    vtkNew<vtkUnstructuredGrid> uGrid;
+    uGrid->SetPoints(points);
+    uGrid->InsertNextCell(hexa->GetCellType(), hexa->GetPointIds());
+    uGrid->InsertNextCell(hexa2->GetCellType(), hexa2->GetPointIds());
+
+    // Extract the outer (polygonal) surface.
+    vtkNew<vtkDataSetSurfaceFilter> surface;
+    surface->SetPieceInvariant(1);
+    surface->SetInputData(uGrid);
+    surface->Update();
+
+    vtkNew<vtkDataSetMapper> aBeamMapper;
+    aBeamMapper->SetInputData(uGrid);
+
+    surface->GetOutput()->PrintSelf(std::cout, vtkIndent(4));
+
+    vtkNew<vtkActor> aBeamActor;
+    aBeamActor->SetMapper(aBeamMapper);
+    aBeamActor->AddPosition(0, 0, 0);
+    aBeamActor->GetProperty()->SetColor(colors->GetColor3d("Yellow").GetData());
+    aBeamActor->GetProperty()->SetOpacity(0.60);
+    aBeamActor->GetProperty()->EdgeVisibilityOn();
+    aBeamActor->GetProperty()->SetEdgeColor(
+        colors->GetColor3d("Black").GetData());
+    aBeamActor->GetProperty()->SetLineWidth(1.5);
+
+    // Create a plane to cut, here it cuts in the XZ direction
+    // (xz normal=(1,0,0); XY =(0,0,1), YZ =(0,1,0)
+    vtkNew<vtkPlane> plane;
+    plane->SetOrigin(0.5, 0, 0);
+    plane->SetNormal(1, 0, 0);
+    // Create cutter
+    vtkNew<vtkCutter> cutter;
+    cutter->SetCutFunction(plane);
+    cutter->SetInputData(aBeamActor->GetMapper()->GetInput());
+    cutter->Update();
+    vtkNew<vtkPolyDataMapper> cutterMapper;
+    cutterMapper->SetInputConnection(cutter->GetOutputPort());
+
+    // Create plane actor
+    vtkNew<vtkActor> planeActor; // 四条线组成的actor
+    planeActor->GetProperty()->SetColor(colors->GetColor3d("Red").GetData());
+    planeActor->GetProperty()->SetLineWidth(2);
+    planeActor->SetMapper(cutterMapper);
+
+    // Create a renderer, render window, and interactor
+    vtkNew<vtkRenderer> renderer;
+    vtkNew<vtkRenderWindow> renderWindow;
+    renderWindow->SetWindowName("DatasetSurface");
+    renderWindow->AddRenderer(renderer);
+    vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+
+    // Add the actors to the scene
+    renderer->AddActor(aBeamActor);
+    renderer->AddActor(planeActor);
+    renderer->SetBackground(colors->GetColor3d("Seashell").GetData());
+
+    renderer->ResetCamera();
+    renderer->GetActiveCamera()->Azimuth(-25);
+    renderer->GetActiveCamera()->Elevation(30);
+
+    // Render and interact
+    renderWindow->Render();
+    renderWindowInteractor->Start();
+
+    return EXIT_SUCCESS;
+}
